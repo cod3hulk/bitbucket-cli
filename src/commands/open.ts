af345abcb0f9e8ecf2d7153a8793f85de54d255d
Tomas Avé <t.ave@mytaxi.com>
2018-11-08 22:49:04 +0100

    Add command to open bitbucket repository

--- src/commands/open.ts
+++ src/commands/open.ts
@@ -1,4 +1,6 @@
 import {Command, flags} from '@oclif/command'
+const opn = require('opn')
+const gitRemoteOriginUrl = require('git-remote-origin-url');
 
 export default class Open extends Command {
   static description = 'Open repository in bitbucket'
@@ -11,19 +13,23 @@ open world from ./src/open.ts!
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    path: flags.string({char: 'd', description: 'path to project'}),
+    path: flags.string({char: 'p', description: 'path to project'}),
+    server: flags.string({char: 's', description: 'bitbucket server'}),
   }
 
   static args = [{name: 'file'}]
 
   async run() {
     const {args, flags} = this.parse(Open)
+    const server = flags.server
+    const path = flags.path
 
-    const name = flags.name || 'world'
-    this.log(`open ${name} from ./src/commands/open.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    gitRemoteOriginUrl(path).then(url => {
+        const matches = url.match(/ssh:\/\/git@.*\/(.*)\/(.*).git/)
+        const project = matches[1]
+        const repository = matches[2]
+        const projectUrl = `${server}/projects/${project}/repos/${repository}/browse`
+        opn(projectUrl, { wait: false })
+    }).catch(error => {console.log(error)});
   }
 }
